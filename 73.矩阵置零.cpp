a6ca7baaf5e6d1fdfcaa365a95db2d16e194cb5b
Xbing <guxinbin2001@gmail.com>
2025-05-17 21:26:50 +0800

    空间复杂度： O(1)

--- 73.矩阵置零.cpp
+++ 73.矩阵置零.cpp
@@ -7,32 +7,62 @@
 // @lc code=start
 #include <unordered_set>
 using namespace std;
-class Solution {
+
+// 直接使用第一行第一列当做之前的两个set
+// 还需要额外两个flag记录第一行第一列是否含有0
+class Solution
+{
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    void setZeroes(vector<vector<int>> &matrix)
+    {
         int m = matrix.size();
         int n = matrix[0].size();
-        unordered_set<int> rows,cols;
+        bool firstRowZero = false, firstColZero = false;
 
-        // 第一次遍历，记录哪些行和列需要被置 0
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (matrix[i][j] == 0) {
-                    rows.insert(i);
-                    cols.insert(j);
+        // 判断首行首列是否需要置零
+        for (int j = 0; j < n; ++j)
+            if (matrix[0][j] == 0)
+                firstRowZero = true;
+
+        for (int i = 0; i < m; ++i)
+            if (matrix[i][0] == 0)
+                firstColZero = true;
+
+        // 填充首行首列
+        for (int i = 1; i < m; ++i)
+        {
+            for (int j = 1; j < n; ++j)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        // 第二次遍历，进行置 0 操作
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (rows.count(i) || cols.count(j)) {
+        // 按照首行首列置零
+        for (int i = 1; i < m; ++i)
+        {
+            for (int j = 1; j < n; ++j)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
                     matrix[i][j] = 0;
                 }
             }
         }
+        // 最后处理首行首列
+        if (firstRowZero)
+        {
+            for (int j = 0; j < n; ++j)
+                matrix[0][j] = 0;
+        }
+        if (firstColZero)
+        {
+            for (int i = 0; i < m; ++i)
+                matrix[i][0] = 0;
+        }
     }
 };
 // @lc code=end
-
