6b1c206277fcab6cd65f91552b0eeff553629c47
Xbing <guxinbin2001@gmail.com>
2025-05-17 21:04:56 +0800

    使用哈希表求解

--- 141.环形链表.cpp
+++ 141.环形链表.cpp
@@ -13,25 +13,24 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <unordered_set>
+using namespace std;
 class Solution
 {
 public:
     bool hasCycle(ListNode *head)
     {
-        if (head == nullptr || head->next == nullptr)
-            return false;
-        ListNode *slow = head;
-        ListNode *fast = head->next;
-
-        while (slow != fast)
+        unordered_set<ListNode *> seen;
+        while (head != nullptr)
         {
-            if (fast == nullptr || fast->next == nullptr)
-                return false;
-
-            slow = slow->next;
-            fast = fast->next->next;
+            if (seen.count(head))
+            {
+                return true;
+            }
+            seen.insert(head);
+            head = head->next;
         }
-        return true;
+        return false;
     }
 };
 // @lc code=end
