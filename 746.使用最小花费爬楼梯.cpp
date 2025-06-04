7d7b5c45020e42f89cb38e04850e032cbd0b13df
Xbing <guxinbin2001@gmail.com>
2025-06-04 21:05:46 +0800

    dp

---
+++ 746.使用最小花费爬楼梯.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=cpp
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        vector<int> dp(cost.size() + 1);
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= cost.size(); i++)
+        {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.size()];
+        // 从第i个台阶向上爬，cost只记录了除顶层之外的所有台阶，所以需要返回dp[cost.size()]
+    }
+};
+// @lc code=end
