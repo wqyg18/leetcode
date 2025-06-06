13ce591a1b16029e30fe5afaa6bc6ce05a61a452
Xbing <guxinbin2001@gmail.com>
2025-06-06 20:33:54 +0800

    使用滚动数组优化dp

--- 62.不同路径.cpp
+++ 62.不同路径.cpp
@@ -11,22 +11,18 @@ class Solution
 public:
     int uniquePaths(int m, int n)
     {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-
-        // 初始化：dp[i][0]一定都是1
-        for (int i = 0; i < m; i++)
-            dp[i][0] = 1;
-        for (int j = 0; j < n; j++)
-            dp[0][j] = 1;
+        // 利用滚动数组优化，只用一维列表实现
+        vector<int> dp(n, 1);
 
         for (int i = 1; i < m; ++i)
         {
             for (int j = 1; j < n; ++j)
             {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                dp[j] = dp[j] + dp[j - 1];
+                // i行j列 = i-1行j列 + i行j-1列
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
 // @lc code=end
